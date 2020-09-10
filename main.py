5f79fe9d9e5cc94a6e32ba68366e061791b24025
Hybrid <56696374+Hybrid3DR@users.noreply.github.com>
2020-09-09 20:33:28 -0700

    Update main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+<<<<<<< Updated upstream
+=======
+import discord
+
+token = open("token.text", "r").read()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):  # event that happens per any message.
+    print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+    if str(message.author) == "Hybrid21#2182" and "hello" in message.content.lower():
+        await message.channel.send('Hi!')
+
+  client.run(token)
+>>>>>>> Stashed changes
