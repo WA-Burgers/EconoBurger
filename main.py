a39878b3f1eceba69b38e68e009ddcb83be959b2
Hybrid <56696374+Hybrid3DR@users.noreply.github.com>
2020-09-11 17:18:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import discord
+import random
 
 from discord.ext import commands
 from keep_alive import keep_alive
@@ -27,6 +29,33 @@ async def on_message(message):
         await message.channel.send('Hi!')
 
     await client.process_commands(message)
+
+    
+@client.command(aliases=['8ball',]) #8ball WOOOOOOO!
+async def _8ball(ctx, *, question):
+      responses = ["It is certain.",
+                  "It is decidedly so.",
+                  "Without a doubt.",
+                  "Yes - definitely.",
+                  "You may rely on it.",
+                  "As I see it, yes.",
+                  "Most likely.",
+                  "Outlook good.",
+                  "Yes.",
+                  "Signs point to yes.",
+                  "Reply hazy, try again.",
+                  "Ask again later.",
+                  "Better not tell you now.",
+                  "Cannot predict now.",
+                  "Concentrate and ask again.",
+                  "Don't count on it.",
+                  "My reply is no.",
+                  "Ask Hybrid about it",
+                  "Ask NullCube about it",
+                  "My sources say no.",
+                  "Outlook not so good.",
+                  "Very doubtful."]
+      await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 
 @client.command()
