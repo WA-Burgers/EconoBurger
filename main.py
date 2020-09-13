226d151eb4680aa5d170bfdb0c64f40664210055
Hybrid <56696374+Hybrid3DR@users.noreply.github.com>
2020-09-12 21:30:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,13 @@ async def change_status():
     
 @client.command(aliases=['8ball',]) #8ball WOOOOOOO!
 async def _8ball(ctx, *, question):
-      await ctx.send(f'Question: ```CSS\n{question}```\nAnswer: ```CSS\n{random.choice(replies)}```')
+      await ctx.send(f'`Question: {question}`\n`Answer: {random.choice(replies)}`')
 
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send(f'`Invalid command used`')
 
 @client.command(aliases = ['purge'])
 async def clear(ctx, amount : int): 
@@ -32,7 +37,7 @@ async def clear(ctx, amount : int):
     if authorperms.manage_messages:
         await ctx.channel.purge(limit=amount)
     else:
-        await ctx.send(f"{random.choice(preplies)}")
+        await ctx.send(f"`{random.choice(preplies)}`")
 
 @clear.error
 async def clear_error(ctx, error):
@@ -43,7 +48,7 @@ async def clear_error(ctx, error):
 @client.command()
 async def ping(ctx):
     """shows the latency between the client and the server in miliseconds"""
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+    await ctx.send(f'`Pong! {round(client.latency * 1000)}ms`')
 
 
 @client.command()
@@ -51,9 +56,9 @@ async def kick(ctx, member : discord.Member, *, reason=None):
     authorperms = ctx.author.permissions_in(ctx.channel)
     if authorperms.kick_members:
         await member.kick(reason=reason)
-        await ctx.send(f'Kicked {member.mention}')
+        await ctx.send(f'`Kicked {member.mention}`')
     else:
-        await ctx.send(f"{random.choice(preplies)}")
+        await ctx.send(f"`{random.choice(preplies)}`")
 
 
 @client.command()
@@ -61,9 +66,9 @@ async def ban(ctx, member : discord.Member, *, reason=None):
     authorperms = ctx.author.permissions_in(ctx.channel)
     if authorperms.ban_members:
         await member.ban(reason=reason)
-        await ctx.send(f'Banned {member.mention}')
+        await ctx.send(f'`Banned {member.mention}`')
     else:
-        await ctx.send(f"{random.choice(preplies)}")
+        await ctx.send(f"`{random.choice(preplies)}`")
 
 
 @client.command()
@@ -78,22 +83,22 @@ async def unban(ctx, *, member):
 
             if (user.name, user.discriminator) == (member_name, member_discriminator):
                 await ctx.guild.unban(user)
-                await ctx.send(f'Unbanned {user.mention}')
+                await ctx.send(f'`Unbanned {user.mention}`')
                 return
     else:
-        await ctx.send(f"{random.choice(preplies)}")
+        await ctx.send(f"`{random.choice(preplies)}`")
 
 
 @client.event
 async def on_member_join(member):
     """logs a message when a user joins the server"""
-    print(f"{member} has joined the server!")
+    await ctx.send(f"`{member} has joined the server!`")
 
 
 @client.event
 async def on_member_remove(member):
     """logs a message when a user leaves the server"""
-    print(f"{member} has left the server.")
+    await ctx.send(f"`{member} has left the server.`")
 
 
 
