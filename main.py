c6dbacc60f163293fa6ce3ed652c34888aa9625c
Hybrid <56696374+Hybrid3DR@users.noreply.github.com>
2020-12-21 09:30:15 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from discord.ext import commands, tasks
 from ballreplies import replies
 from workreplies import wreplies
 from random import choice
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from os import system
 from itertools import cycle
 from permissiondeniedreplies import preplies
 
@@ -577,19 +580,34 @@ async def remove(ctx, number):
     except:
         await ctx.send('Your queue is either **empty** or the index is **out of range**')
         
-@client.command(name='play', help='This command plays songs')
-async def play(ctx):
-    global queue
-
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-
-    async with ctx.typing():
-        player = await YTDLSource.from_url(queue[0], loop=client.loop)
-        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-
-    await ctx.send('**Now playing:** {}'.format(player.title))
-    del(queue[0])
+@client.command(pass_context=True, brief="This will play a song 'play [url]'", aliases=['pl'])
+async def play(ctx, url: str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music end or use the 'stop' command")
+        return
+    await ctx.send("Getting everything ready, playing audio soon")
+    print("Someone wants to play music let me get that ready for them...")
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, 'song.mp3')
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    voice.volume = 100
+    voice.is_playing()
 
 @client.command(name='pause', help='This command pauses the song')
 async def pause(ctx):
